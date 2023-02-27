ae9fdc1c104bb7ae3fc304c55c37665505fd9985
andhika.adhitia96 <andhika.adhitia96@gmail.com>
2023-02-27 16:13:22 +0700

    First Commit 27022023

--- application/logs/log-2023-01-21.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-DEBUG - 2023-01-21 01:39:20 --> UTF-8 Support Enabled
-DEBUG - 2023-01-21 01:39:21 --> UTF-8 Support Enabled
-DEBUG - 2023-01-21 01:39:37 --> UTF-8 Support Enabled
